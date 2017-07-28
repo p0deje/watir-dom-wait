1337c446f106748810b3e8cc1e16d02eeba25745
Alex Rodionov <p0deje@gmail.com>
2017-07-28 17:51:26 +0600

    Revert "Chrome on Travis no longer needs --no-sandbox switch"
    This reverts commit f4ecaafbedbcc9a43028f82a511253e0e577ce76.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,12 @@ RSpec.configure do |spec|
   spec.filter_run_excluding bug: /\d+/
 
   spec.before(:all) do
-    Selenium::WebDriver::Chrome.path = "#{File.dirname(__FILE__)}/../bin/google-chrome" if ENV['TRAVIS']
-    @browser = Watir::Browser.new(:chrome)
+    opts = {}
+    if ENV['TRAVIS']
+      Selenium::WebDriver::Chrome.path = "#{File.dirname(__FILE__)}/../bin/google-chrome"
+      opts[:args] = ['no-sandbox']
+    end
+    @browser = Watir::Browser.new(:chrome, opts)
     @browser.goto "data:text/html,#{File.read('spec/support/html/wait_for_dom.html')}"
   end
 
