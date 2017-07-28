dbb08e69f4c80b8c611003a68c59a683055da05f
Alex Rodionov <p0deje@gmail.com>
2017-07-28 17:44:46 +0600

    Chrome on Travis no longer needs --no-sandbox switch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,7 @@ RSpec.configure do |spec|
   spec.filter_run_excluding bug: /\d+/
 
   spec.before(:all) do
-    opts = {}
-    if ENV['TRAVIS']
-      Selenium::WebDriver::Chrome.path = "#{File.dirname(__FILE__)}/../bin/google-chrome"
-      opts[:args] = ['no-sandbox']
-    end
+    Selenium::WebDriver::Chrome.path = "#{File.dirname(__FILE__)}/../bin/google-chrome" if ENV['TRAVIS']
     @browser = Watir::Browser.new(:chrome, opts)
     @browser.goto "data:text/html,#{File.read('spec/support/html/wait_for_dom.html')}"
   end
